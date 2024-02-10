339b3e7a8590c6ea152e25f31405aa81b76c4e73
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-09 23:18:15 -0800

    initial commit of python programs

---
+++ beginning.py
@@ -0,0 +1,41 @@
+# Arrays (called lists in python)
+arr = [1,2,3]
+print(arr)
+
+# can be used as a stack
+arr.append(4)
+arr.append(5)
+print(arr)
+
+arr.pop()
+print(arr)
+
+# insert is O(n)
+arr.insert(1,7) #since it's an array you can insert at pos, value
+print(arr)
+
+# random value access is O(1)
+arr[0] = 0
+arr[3] = 0
+print(arr)
+
+# init arr of size n with default value of 1
+n = 5
+arr = [1] * n
+print(arr)
+print(len(arr))
+
+# -1 is not out of bounds, it's the last value
+arr = [1,2,3]
+print(arr[-1])
+
+# sublists (aka slicing) first value inclusive, last exclusive
+arr = [1,2,3,4]
+print(arr[1:3])
+
+# similar to for-loop ranges, last index is non-inclusive
+print(arr[0:4])
+
+# unpacking is assigning values from array to variable, but need even amnt
+a,b,c = [1,2,3]
+print(f'{a} {b} {c}')
