083647ea17374b480bcc56f4f7de843fd833057d
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 11:33:19 -0800

    lists practice complete

--- lists.py
+++ lists.py
@@ -21,5 +21,35 @@ print(singer1)
 print(singers[2])
 print(singers[len(singers)-1])
 
+motorcycles = ["Honda", "Yamaha", "Suzuki"]
+print(motorcycles[0])
+
+motorcycles[0] = "Ducati"
+print(motorcycles[0])
 
 # modifying lists
+aliens = []
+aliens.append("E.T")
+aliens.append("Goku")
+aliens.append("Marvin")
+aliens.append("Gazoo")
+print(aliens[1])
+aliens.pop(1)
+print(aliens[1])
+print(aliens)
+
+aliens.append("Goku")
+i = 0
+while(i < len(aliens)):
+    print(aliens[i])
+    i = i+1
+
+
+colors = ["Yellow", "Blue", "Red"]
+print(colors[len(colors)-1])
+
+secretMessage = ["Planet", "You", "Secret", "Are", "Zombie", "The", "Nuclear", "Alien"]
+i = 1
+while(i < len(secretMessage)):
+    print(secretMessage[i])
+    i = i+2
