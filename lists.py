140c4e7b91fb50a2cadc06bc88f7d8dbe9b4d1d1
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 11:06:11 -0800

    started lists

---
+++ lists.py
@@ -0,0 +1,25 @@
+# Practice with Lists in Python
+
+# Creation
+names = ["Peter", "Yuliana", "Valeria"]
+print(names)
+print(type(names))
+
+# print data list
+myData = ["Luisa", 37, "Imalvarez33@eafit.edu.co", 164.1]
+print(myData)
+for data in myData:
+    print(type(data))
+    print(data)
+print(len(myData))
+
+singers = ["Shakira", "Karol G", "Feid", "Maluma"]
+singer1 = singers[0]
+
+print(len(singers))
+print(singer1)
+print(singers[2])
+print(singers[len(singers)-1])
+
+
+# modifying lists
