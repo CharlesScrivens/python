7a7f72588a37e19e7b39edcd56f80de2ffb3bb68
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 11:36:20 -0800

    Dictionaries practice begin

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries practice
+product = {
+    "name": "iPhone 14",
+    "price": 799,
+    "color": "midnight"
+}
+
+print(product)
+print(type(product))
+
+# lists are best used for storing ordered data of the same time
+# dictionary best used for storing data where the order isn't important
