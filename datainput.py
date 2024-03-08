4e62d5732c2db277e855bb6ba0e5d3fecd6fd7cd
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 10:03:40 -0800

    more practice with input/conditionals

--- datainput.py
+++ datainput.py
@@ -1,5 +1,11 @@
 #data input/output
-print("Enter your name: ")
-name = input()
-print(f"Your name is: {name} ")
+
+#print("Enter your name: ")
+#name = input()
+#print(f"Your name is: {name} ")
 #print(name)
+
+# number input
+age = int(input("Enter your age: "))
+halfAge = age/2
+print(halfAge)
---
+++ simpleconditionals.py
@@ -0,0 +1,18 @@
+# Chapter 7 simple conditionals
+
+#side = float(input("Enter the length of the square's side: "))
+#area = side*side
+#print("The area of the square is: " + str(area))
+
+
+temperature = float(input("Enter the Temperature: "))
+if(temperature > 27):
+    print("Buy ice cream")
+elif(temperature < 15):
+    print("Buy chocolate")
+else:
+    print("Buy OJ")
+    
+print("End of program")
+
+
