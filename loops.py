fbf534abf5e2dd89cdcb5298cf5959c03e3d28f9
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 10:13:17 -0800

    Strings practice - Basic

---
+++ loops.py
@@ -0,0 +1,15 @@
+#loops practice
+i = 0
+#while(i <= 3):
+    #print("Welcome to Python")
+#    print(i)
+#    i = i+1
+name = ""
+while(name != "Daniel"):
+    name = input("Enter a name: ")
+    i = i+1
+    if(i == 3):
+        print("Game over")
+        break
+
+print("End of Program")
---
+++ strings.py
@@ -0,0 +1,8 @@
+# practice messing with strings
+firstName = "John"
+lastName = "Snow"
+fullName = firstName + " " + lastName
+
+print(fullName)
+print("30 years old")
+print(type(fullName))
