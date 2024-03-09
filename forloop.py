66f5b31e3523fad1443f1ae2d340d72db77bf0bd
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 17:48:47 -0800

    finished for loops

--- forloop.py
+++ forloop.py
@@ -28,3 +28,36 @@ nightclub2 = {'name':'teatro victoria','location':'poblado'}
 nightclubs = [nightclub1, nightclub2]
 for nightclub in nightclubs:
     print(nightclub['name'] + " - " + nightclub['location'])
+
+# range function (can have single number which it goes to, 2 numbers for start and [end-1] and 3 to have a counter
+for number in range(6):
+    print(number)
+
+for number in range(1,7,2):
+    print(number)
+
+for number in range(1,81):
+    print(number)
+
+for number in range(1000,0,-100):
+    print(number)
+
+for number in range(2,68,2):
+    print(number)
+
+name1 = input("Enter name 1: ")
+name2 = input("Enter name 2: ")
+name3 = input("Enter name 3: ")
+name4 = input("Enter name 4: ")
+
+names = []
+names.append(name1)
+names.append(name2)
+names.append(name3)
+names.append(name4)
+
+for name in names:
+    if name[0] == "L" or name[0] == 'l':
+        print("Name discarded")
+    else:
+        print(name)
