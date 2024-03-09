7a611fa52c678cbd4033eb89c1a02ce5dfa69497
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-08 18:14:44 -0800

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# more practice with functions
+def add():
+    num1 = float(input('enter first num: '))
+    num2 = float(input('enter second num: '))
+    result = num1 + num2
+    print(f'result is: {result}')
+
+def subtract():
+    num1 = float(input('enter first num: '))
+    num2 = float(input('enter second num: '))
+    result = num1 - num2
+    print(f'result is: {result}')
+
+while(True):
+    print('-----Enter the operation----')
+    print('0) Exit')
+    print('1) Add')
+    print('2) Subtract')
+    operation = int(input())
+    if(operation == 0):
+        print('byeeee')
+        break
+    elif(operation == 1):
+        add()
+    elif(operation == 2):
+        subtract()
+    else:
+        print('select a correct choice')
---
+++ functions.py
@@ -0,0 +1,24 @@
+# practice with functions
+def greet():
+    print("Welcome")
+    print("To my program")
+
+def customGreeting(firstName, lastName):
+    print(f'Hello {firstName} {lastName}')
+
+customGreeting('Sinan', 'Boo')
+customGreeting('Choi', 'Kimukimu')
+
+
+greet()
+print("EoP")
+
+def calculateFine(daysLate):
+    if(daysLate>7):
+        return daysLate*2
+    else:
+        return 3
+
+days = int(input('Enter the num days late: '))
+fine = calculateFine(days)
+print('You must pay a fine of ' + str(fine) + ' dollars')
